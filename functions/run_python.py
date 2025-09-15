de7206acfaefde4e818a9451d02031323fb9e629
avgra3 <antonygradillas@gmail.com>
2025-09-15 12:26:03 -0700

    Addition of an exectution method

---
+++ functions/run_python.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from subprocess import run, CalledProcessError
+
+
+def run_python_file(working_directory, file_path, args=[]):
+    path_to_check = Path(working_directory).absolute() / file_path
+    if not str(path_to_check.resolve()).startswith(
+        str(Path(working_directory).absolute())
+    ):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+
+    if not path_to_check.exists():
+        return f'Error: File "{file_path}" not found.'
+    if not path_to_check.as_posix().endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+    # subprocess.run(args, *, stdin=None, input=None, stdout=None, stderr=None, capture_output=False, shell=False, cwd=None, timeout=None, check=False, encoding=None, errors=None, text=None, env=None, universal_newlines=None, **other_popen_kwargs)
+    err = ""
+    try:
+        output = run(
+            ["uv", "run", file_path, *args],
+            cwd=working_directory,
+            timeout=30,
+            capture_output=True,
+        )
+    except CalledProcessError as e:
+        err = f"Process exited with code {e.returncode}"
+    if output.stderr == "" and output.stdout == "" and err == "":
+        return "No output produced"
+    outmessage = f"STDOUT: {output.stdout}" + "\n" + f"STDERR: {output.stderr}"
+    if len(err) > 0:
+        outmessage += "\n" + err
+    return outmessage
--- tests.py
+++ tests.py
@@ -1,15 +1,29 @@
 # from functions.get_files_info import get_file_content
-from functions.write_file import write_file
+# from functions.write_file import write_file
+from functions.run_python import run_python_file
 
 results = [
-    write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"),
-    write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"),
-    write_file("calculator", "/tmp/temp.txt", "this should not be allowed"),
+    run_python_file("calculator", "main.py"),
+    run_python_file("calculator", "main.py", ["3+5"]),
+    run_python_file("calculator", "tests.py"),
+    run_python_file("calculator", "../main.py"),
+    run_python_file("calculator", "nonexistent.py"),
 ]
 
 for result in results:
     print(result)
 
+
+# Goes with write_file
+# results = [
+#     write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"),
+#     write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"),
+#     write_file("calculator", "/tmp/temp.txt", "this should not be allowed"),
+# ]
+#
+# for result in results:
+#     print(result)
+#
 
 # Goes with get_file_content
 # print(get_file_content("calculator", "lorem.txt"))
